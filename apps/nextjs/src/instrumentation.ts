8cce36d0ef3fc7be03cfc8aaad17333bfbb18cdd
dkimura <dkimura@bdd.xyz>
2024-12-01 02:26:09 +0900

    fix(instrumentation.ts): log NEXT_RUNTIME environment variable for better debugging and ensure correct runtime handling

--- apps/nextjs/src/instrumentation.ts
+++ apps/nextjs/src/instrumentation.ts
@@ -1,6 +1,6 @@
-export const runtime = 'nodejs'
-
 export async function register() {
+  console.log('instrumentationruntime', process.env.NEXT_RUNTIME)
+
   if (process.env.NEXT_RUNTIME === 'nodejs') {
     const { initMocks } = await import('./testing/msw')
     await initMocks()
