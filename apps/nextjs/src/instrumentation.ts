fecce63b5a9c5d955e41cda4165124c395cc30d1
dkimura <dkimura@bdd.xyz>
2024-12-01 01:08:03 +0900

    fix(instrumentation.ts): update initMocks call to await for proper execution in nodejs runtime

--- apps/nextjs/src/instrumentation.ts
+++ apps/nextjs/src/instrumentation.ts
@@ -1,6 +1,8 @@
+export const runtime = 'nodejs'
+
 export async function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
     const { initMocks } = await import('./testing/msw')
-    initMocks()
+    await initMocks()
   }
 }
